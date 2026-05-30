179f623713ad12e1250880b9261fb8f3379d301f
Preetam <vpulla@asu.edu>
2026-05-30 13:53:39 -0700

    Added validation for the request dto

--- src/main/java/com/example/credit/customer/CustomerRequestDTO.java
+++ src/main/java/com/example/credit/customer/CustomerRequestDTO.java
@@ -1,10 +1,21 @@
 package com.example.credit.customer;
 
+import jakarta.validation.constraints.*;
+
 public record CustomerRequestDTO(
+        @NotBlank
         String firstName,
+        @NotNull
         String lastName,
+        @Email
+        @NotNull
         String email,
+        @Size(
+                max = 20, message = "Phone number must not exceed 20 characters"
+        )
         String phoneNumber,
+
+        @Past(message = "Date of birth must be in the past")
         String DOB
 ) {
 }
