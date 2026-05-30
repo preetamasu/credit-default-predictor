0fe4bbf373a7da315bc6fd28af185492cb00d977
Preetam <vpulla@asu.edu>
2026-05-30 13:49:29 -0700

    Added custom methods in customer repo

--- src/main/java/com/example/credit/customer/CustomerRepository.java
+++ src/main/java/com/example/credit/customer/CustomerRepository.java
@@ -2,7 +2,10 @@ package com.example.credit.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    List<Customer> findByCustomerStatusNot(CustomerStatus customerStatus);
+    boolean existsByEmail(String email);
 }
