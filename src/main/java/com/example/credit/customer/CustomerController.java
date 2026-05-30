98dbc3024bedda310629607b007c7406dfb6cca2
Preetam <vpulla@asu.edu>
2026-05-30 13:39:31 -0700

    Done with the simple controller file for customer

--- src/main/java/com/example/credit/customer/CustomerController.java
+++ src/main/java/com/example/credit/customer/CustomerController.java
@@ -1,4 +1,33 @@
 package com.example.credit.customer;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+    public final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody
+                                                              CustomerRequestDTO customerRequestDTO){
+        return new ResponseEntity<>(customerService.createCustomer(customerRequestDTO), HttpStatus.CREATED);
+    }
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerResponseDTO> getCustomerById(@PathVariable UUID customerId){
+        return new ResponseEntity<>(customerService.getCustomerById(customerId),HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers(){
+     return new ResponseEntity<>(customerService.getAllCustomers(),HttpStatus.OK);
+    }
 }
