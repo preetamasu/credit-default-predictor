64a984ce6c25cff188be0e6fa40a8a82d64ec355
Preetam <vpulla@asu.edu>
2026-06-07 18:12:21 -0700

    changes to the user class

--- src/main/java/com/example/credit/user/User.java
+++ src/main/java/com/example/credit/user/User.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -18,7 +19,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name="users a")
+@Table(name="users")
 public class User implements UserDetails {
 
     @Id
@@ -34,6 +35,10 @@ public class User implements UserDetails {
 
     private boolean enabled;
 
+
+    @Enumerated(
+            EnumType.STRING
+    )
     private Role role;
 
     private Date createdAt;
@@ -43,7 +48,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(
+                new SimpleGrantedAuthority(role.name())
+        );
     }
 
     @Override
