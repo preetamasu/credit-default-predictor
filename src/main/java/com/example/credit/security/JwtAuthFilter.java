cfd6dbeae14d1a676915d9c54fbbae56ed5e0343
Preetam <vpulla@asu.edu>
2026-06-08 16:24:01 -0700

    Added shouldNotFilter method to the jwt filter

--- src/main/java/com/example/credit/security/JwtAuthFilter.java
+++ src/main/java/com/example/credit/security/JwtAuthFilter.java
@@ -47,4 +47,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         filterChain.doFilter(request,response);
 
     }
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getServletPath().startsWith("/api/v1/auth/");
+    }
 }
