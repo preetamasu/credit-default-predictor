c1a6da242e59368dedce312faf13bd14bdb512a8
Preetam <vpulla@asu.edu>
2026-06-08 15:39:45 -0700

    Completed security config file

--- src/main/java/com/example/credit/security/SecurityConfig.java
+++ src/main/java/com/example/credit/security/SecurityConfig.java
@@ -1,13 +1,32 @@
 package com.example.credit.security;
 
+import com.example.credit.user.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                auth-> auth.requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+        )
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 
     }
 }
